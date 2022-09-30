eab26b222fc9d66432e294c5b439db41765a7b2a
duval-justine <95071327+duval-justine@users.noreply.github.com>
2022-09-30 16:07:04 +0200

    Importation de différents éléments du squelette

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,8 @@
 <script setup>
 import SignIn from './components/SignIn.vue'
+import FamousPoets from './components/FamousPoets.vue'
 import { createClient } from '@supabase/supabase-js'
 import { SupabaseAuthClient } from '@supabase/supabase-js/dist/module/lib/SupabaseAuthClient'
-
-const SUPABASE_URL = 'https://idgwvxhrjdggndemqwpo.supabase.co'
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlkZ3d2eGhyamRnZ25kZW1xd3BvIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjM1Nzg4NjYsImV4cCI6MTk3OTE1NDg2Nn0.oCy1yz68m1eqCMzJMd8yGTzvCNOcG7p1_Prwh3Rj_6Y'
-const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
 
 supabase.auth.onAuthStateChange((event,session) => {
   if(session==null){
@@ -39,92 +36,156 @@ async function logout(){
 <template>    
   <header>
     <router-link to="/">Go to Home</router-link>
-    <img alt="Logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-      <h1>{{msg}}</h1>
-  <p>
-    Please login if you have an account or register :
-  </p>
-  <button @click="login()">Sign In</button><br>
-  <button @click="logout()">Sign Out</button><br>
-  <label id="status">You are not yet logged ! </label>
+    <img alt="Poetry" class="logo" src="./assets/logo.png" width="125" height="125" />
+    <div class="wrapper" id="signOut">
+      <div><SignIn msg="Poet ! Tell us who you are !" /></div>
+      <label>email: </label><br>
+	    <input type="email" required v-model="email" placeholder="username@domain.tld"><br>
+	    <label>password: </label><br>
+	    <input type="password" required v-model="passwd" ><br>
+      <button v-on:click="register()">Sign Up</button>
+      <button v-on:click="login()">Sign In</button>
+      
+    </div>
+    <div class="hidden" id="addPoem">
+      <div><SignIn msg="Write your poem !" /></div>
+      <h3>The poem remains private, until you make it public</h3>
+      <label>Poem's title</label><br>
+	      <input type="text" required name="title" v-model="title" placeholder="edit me"><br>
+	      <label>Poem's content</label><br>
+        <textarea required name="content" v-model="content" placeholder="edit me" rows="10" cols="50">Your poem here ...
+        </textarea> <br>
+        <label>Illustration: </label>
+        
+	      <input type="file"  id="file" name="file" placeholder="my file" accept="image/png, image/jpeg"><br>
+	      <!--<img id="illustration" src="./assets/null.png" alt="poem illustration" width="75" height="75"/><br>-->
+        <input type="checkbox" v-model="hidden" value=true/>
+        <label>Hidden poem</label>
+      <br><button v-on:click="createPoem()">Add the poem</button>
+      <button v-on:click="fetchPoems()">List of poems</button><br>
+      <label for="poemtitle" id="poemtitle" style="color: teal;font-weight: 500;"> ... </label> 
+      <img id="poemillustration" src="./assets/null.jpg" alt="poem illustration" width="75" height="75" style="background-color:gray;"/><br>
+      <textarea  id="poemcontent" readonly rows="10" cols="50"> ... </textarea> <br>
+      <button v-on:click="nextPoem()">Next poem</button><br>
+      
+    </div>
+    
   </header>
   
   <main>
-    
+    <FamousPoets />
   </main>
 </template>
 
+<script>
+
+  const SUPABASE_URL = 'https://idgwvxhrjdggndemqwpo.supabase.co'
+  const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlkZ3d2eGhyamRnZ25kZW1xd3BvIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjM1Nzg4NjYsImV4cCI6MTk3OTE1NDg2Nn0.oCy1yz68m1eqCMzJMd8yGTzvCNOcG7p1_Prwh3Rj_6Y'
+  const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
+
+  export default {
+  methods: {  
+    //this method allows a new user to sign up the system. Once done, the user receives an email
+    //asking for account validation. Once the validation made the user is added to the system
+    async register(){
+
+    },
+    //this method allows the already registred user to log in the system.
+    //only authenticated users can later add or read the poems
+    async login(){
+
+    },
+    //this method allows to add new poem for the authenticated user (after sign in) 
+    //it is called when the user click on the add poem button after being entered
+    //the title, the content, the visibility and the associated illustration
+    async createPoem(){
+    
+    },
+    //this method allows to extract all readable poems of the authenticated user
+    //including his peoms and the not hidden poems. This policy is implemented by the supabase system 
+    async fetchPoems(){
+        
+    },
+    //this function allows to display the next accessibe poem for the current user
+    //the fetch button should be selected before
+    nextPoem(){
+    
+    }
+  }  
+}
+</script>
 
 
 <style>
-@import './assets/base.css';
-
-header .hidden {
-    visibility: hidden;
-    overflow: hidden;
-    display: flex;
-    display:inline-block;
-    place-items: flex-start;
-    flex-wrap: wrap;
-}
-
-#app {
-  max-width: 1280px;
-  margin: 0 auto;
-  padding: 2rem;
-
-  font-weight: normal;
-}
-
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-a,
-.green {
-  text-decoration: none;
-  color: hsla(160, 100%, 37%, 1);
-  transition: 0.4s;
-}
-
-@media (hover: hover) {
-  a:hover {
-    background-color: hsla(160, 100%, 37%, 0.2);
+  @import './assets/base.css';
+  
+  header .hidden {
+      visibility: hidden;
+      overflow: hidden;
+      display: flex;
+      display:inline-block;
+      place-items: flex-start;
+      flex-wrap: wrap;
   }
-}
-
-@media (min-width: 1024px) {
-  body {
-    display: flex;
-    place-items: center;
-  }
-
+  
   #app {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    padding: 0 2rem;
+    max-width: 1280px;
+    margin: 0 auto;
+    padding: 2rem;
+  
+    font-weight: normal;
   }
-
+  
   header {
-    display: inline-block;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+    line-height: 1.5;
   }
-
-  header .wrapper {
-    display: flex;
-    display:inline-block;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
+  
   .logo {
-    margin: 0 2rem 0 0;
+    display: block;
+    margin: 0 auto 2rem;
   }
-}
-</style>
+  
+  a,
+  .green {
+    text-decoration: none;
+    color: hsla(160, 100%, 37%, 1);
+    transition: 0.4s;
+  }
+  
+  @media (hover: hover) {
+    a:hover {
+      background-color: hsla(160, 100%, 37%, 0.2);
+    }
+  }
+  
+  @media (min-width: 1024px) {
+    body {
+      display: flex;
+      place-items: center;
+    }
+  
+    #app {
+      display: grid;
+      grid-template-columns: 1fr 1fr;
+      padding: 0 2rem;
+    }
+  
+    header {
+      display: inline-block;
+      place-items: center;
+      padding-right: calc(var(--section-gap) / 2);
+    }
+  
+    header .wrapper {
+      display: flex;
+      display:inline-block;
+      place-items: flex-start;
+      flex-wrap: wrap;
+    }
+  
+    .logo {
+      margin: 0 2rem 0 0;
+    }
+  }
+  </style>
+  
